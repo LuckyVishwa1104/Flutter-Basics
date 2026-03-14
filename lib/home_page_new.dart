4a950567867fee54e1577d73a9d37794108cc5af
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-03-14 18:22:38 +0530

    feat(text): text style and its properties

--- lib/home_page_new.dart
+++ lib/home_page_new.dart
@@ -8,10 +8,12 @@ class HomePageNew extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-
-        // App Bar - 
+        // App Bar -
         appBar: AppBar(
-          title: const Text('New App Bar', style: TextStyle(fontSize: 25),),
+          title: const Text(
+            'New App Bar',
+            style: TextStyle(fontSize: 25),
+          ),
           backgroundColor: const Color.fromARGB(255, 227, 226, 226),
           foregroundColor: const Color.fromARGB(255, 117, 39, 32),
           // centerTitle: true,
@@ -19,11 +21,59 @@ class HomePageNew extends StatelessWidget {
         ),
 
         // Drawer
-        drawer: const Icon(Icons.grid_3x3,),
+        drawer: const Icon(
+          Icons.grid_3x3,
+        ),
 
         // Body -
-        
+        body: const Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            
+            Text(
+              'Good Afternoon Flutter',
+              style: TextStyle(
+                fontSize: 30,  // to control the size of the text
+                fontWeight: FontWeight.w700, // to contorl the thickness of the text
+                fontStyle: FontStyle.italic, // to cpontrol the font style of the text (italic, normal)
+                color: Colors.red, // to change the color of the text
+                letterSpacing: 2.3, // to provide spacing between the character of the text
+                wordSpacing: 4, // to provide spaceing between the words of the text
+                decoration: TextDecoration.overline, // to add line to the text (under, over, inline)
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
 
+            Text(
+              'Good Morning Flutter',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w300,
+                fontStyle: FontStyle.normal,
+                decoration: TextDecoration.lineThrough,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+
+            Text(
+              'Good Night Flutter',
+              style: TextStyle(
+                fontSize: 25,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+                color: Colors.blue,
+                letterSpacing: 3,
+                wordSpacing: 2,
+                decoration: TextDecoration.underline,
+              ),
+            ),
+            
+          ],
+        ),
       ),
     );
   }
