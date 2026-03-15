285f40c8e44e7cd1eb15b2702ff3d4a84a2cd7a6
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-03-15 23:49:56 +0530

    feat(bottom-bar): add botto navigation bar with respective parameter

--- lib/home_page_new.dart
+++ lib/home_page_new.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HomePageNew extends StatelessWidget {
+class HomePageNew extends StatefulWidget {
   const HomePageNew({super.key});
+
+  @override
+  State<HomePageNew> createState() => _HomePageNewState();
+}
+
+class _HomePageNewState extends State<HomePageNew> {
+  int currentIndexNew = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -26,64 +33,133 @@ class HomePageNew extends StatelessWidget {
         ),
 
         // Body -
-        body: const Column(
+        body: Column(
           // column take all available height and take max width of tis children
-          
 
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           // crossAxisAlignment: CrossAxisAlignment.stretch,
 
           // check for - spaceAround
-          
+
           children: [
-            
-            Text(
-              'Good Afternoon Flutter',
-              style: TextStyle(
-                fontSize: 30,  // to control the size of the text
-                fontWeight: FontWeight.w700, // to contorl the thickness of the text
-                fontStyle: FontStyle.italic, // to cpontrol the font style of the text (italic, normal)
-                color: Colors.red, // to change the color of the text
-                letterSpacing: 2.3, // to provide spacing between the character of the text
-                wordSpacing: 4, // to provide spaceing between the words of the text
-                decoration: TextDecoration.overline, // to add line to the text (under, over, inline)
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+              ),
+              child: const Text(
+                'Good Afternoon Flutter',
+                style: TextStyle(
+                  fontSize: 30, // to control the size of the text
+                  fontWeight:
+                      FontWeight.w700, // to contorl the thickness of the text
+                  fontStyle: FontStyle
+                      .italic, // to cpontrol the font style of the text (italic, normal)
+                  color: Colors.red, // to change the color of the text
+                  letterSpacing:
+                      2.3, // to provide spacing between the character of the text
+                  wordSpacing:
+                      4, // to provide spaceing between the words of the text
+                  decoration: TextDecoration
+                      .overline, // to add line to the text (under, over, inline)
+                ),
               ),
             ),
             // SizedBox(
             //   height: 20,
             // ),
-        
-            Text(
-              'Good Morning Flutter',
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.w300,
-                fontStyle: FontStyle.normal,
-                decoration: TextDecoration.lineThrough,
+
+            Container(
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.black)),
+              child: const Text(
+                'Good Morning Flutter',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w300,
+                  fontStyle: FontStyle.normal,
+                  decoration: TextDecoration.lineThrough,
+                ),
               ),
             ),
             // SizedBox(
             //   height: 20,
             // ),
-        
-            Text(
-              'Good Night Flutter',
-              style: TextStyle(
-                fontSize: 25,
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.w500,
-                color: Colors.blue,
-                letterSpacing: 3,
-                wordSpacing: 2,
-                decoration: TextDecoration.underline,
+
+            Container(
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.black)),
+              child: const Text(
+                'Good Morning Flutter',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w300,
+                  fontStyle: FontStyle.normal,
+                  decoration: TextDecoration.lineThrough,
+                ),
               ),
             ),
-        
+            // SizedBox(
+            //   height: 20,
+            // ),
+
+            Container(
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.black)),
+              child: const Text(
+                'Good Night Flutter',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.blue,
+                  letterSpacing: 3,
+                  wordSpacing: 2,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
           ],
+        ),
+
+        // BOttom nativagtion Bar
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: const Color.fromARGB(255, 169, 169, 169),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: Colors.black,
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.settings,
+                color: Colors.black,
+              ),
+              label: 'Settings',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                color: Colors.black,
+              ),
+              label: 'Account',
+            ),
+          ],
+
+          currentIndex: currentIndexNew,
+
+          // ontap functoin for tab changing
+
+          onTap: (int abc){
+            setState(() {
+              currentIndexNew = abc;
+            });
+          },
+
         ),
       ),
     );
   }
 }
-
-
