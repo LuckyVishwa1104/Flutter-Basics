52423d45f5a12513d0c596673e87ed35e3f5fd70
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-03-30 07:49:56 +0530

    feat(column): add main and cross axis parameters

--- lib/constructs/custom_column_demo.dart
+++ lib/constructs/custom_column_demo.dart
@@ -33,46 +33,103 @@ class CustomColumnDemo extends StatelessWidget {
       ),
 
       // body
-      body: Column(
-        children: [
-
-          // 1st children
-          Container(
-            height: 80,
-            width: 80,
-            decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(
-                8,
-              ),
-            ),
-          ),
-
-          // 2nd children
-          Container(
-            height: 80,
-            width: 80,
-            decoration: BoxDecoration(
-              color: Colors.green,
-              borderRadius: BorderRadius.circular(
-                8,
-              ),
-            ),
-          ),
-
-          // 3rd children
-          Container(
-            height: 80,
-            width: 80,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(
-                8,
-              ),
-            ),
-          )
-        ],
+      body: Container(
+        width: double.infinity,
+        child: Column(
         
+          // MainAxisAlignment : for column it deal with vertical axis
+          /**
+           * MainAxisAlignment.start - it align the children to the start of the column
+           * MainAxisAlignment.center - it align the element to the center of the column
+           * MainAxisAlignment.end - it align the element of the end of the column
+           * 
+           * MainAxisAlignment.spaceEvenly - it will add equal spaces between the middle and edge children of the column
+           * MainAxisAlignment.spaceBetween - it will add equal spaces between the chidlrens eccept the edge childrens
+           * MainAxisAlignment.spaceAround - it will add equal space between the middle childrens and half spaces around the edge of the column
+           */
+        
+          // CrossAxisAlignment : for column it deal with horizontal axis
+          /**
+           * CrossAxisAlignment.start - it will align the children to the start of the parent widget
+           * CrossAxisAlignment.center - it will align the children to the center of the parent widget
+           * CrossAxisAlignment.end - it will align the children to the end of the parent widget
+           * 
+           * CrossAxisAlignment.strech - it will force the children to take max available width of parent widget
+           * CrossAxisAlignment
+           */
+        
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        
+          // crossAxisAlignment: CrossAxisAlignment.start,
+
+          // textBaseline: TextBaseline.alphabetic,
+
+          children: [
+
+            Container(
+              width: 200,
+                decoration: const BoxDecoration(color: Color.fromARGB(255, 169, 169, 169),),
+                child: const Text(
+                  "Hello",
+                  style: TextStyle(fontSize: 40, color: Colors.blue),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Container(
+                decoration: const BoxDecoration(color: Color.fromARGB(255, 169, 169, 169),),
+                child: const Text(
+                  "World",
+                  style: TextStyle(fontSize: 20, color: Colors.red),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Container(
+                decoration: const BoxDecoration(color: Color.fromARGB(255, 169, 169, 169),),
+                child: const Text(
+                  "Flutter",
+                  style: TextStyle(fontSize: 30, color: Colors.green),
+                ),
+              ),
+        
+            // // 1st children
+            // Container(
+            //   height: 80,
+            //   width: 80,
+            //   decoration: BoxDecoration(
+            //     color: Colors.red,
+            //     borderRadius: BorderRadius.circular(
+            //       8,
+            //     ),
+            //   ),
+            // ),
+        
+            // // 2nd children
+            // Container(
+            //   height: 80,
+            //   width: 80,
+            //   decoration: BoxDecoration(
+            //     color: Colors.green,
+            //     borderRadius: BorderRadius.circular(
+            //       8,
+            //     ),
+            //   ),
+            // ),
+        
+            // // 3rd children
+            // Container(
+            //   height: 80,
+            //   width: 80,
+            //   decoration: BoxDecoration(
+            //     color: Colors.blue,
+            //     borderRadius: BorderRadius.circular(
+            //       8,
+            //     ),
+            //   ),
+            // )
+            
+          ],
+          
+        ),
       ),
     );
   }
