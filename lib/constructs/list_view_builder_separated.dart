8243317d3329429960c59c879f5edc507b2b2efd
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-04-04 21:56:32 +0530

    feat(list-view-separator): implement ListViewSeparator

---
+++ lib/constructs/list_view_builder_separated.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class ListViewBuilderSeparated extends StatelessWidget {
+  const ListViewBuilderSeparated({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    List<Color> containerColor = [
+      Colors.purple,
+      Colors.indigo,
+      Colors.blue,
+      Colors.green,
+      Colors.yellow,
+      Colors.orange,
+      Colors.red,
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        // foregrand and background color
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+
+        // app bar title
+        title: const Text(
+          'ListViewSeparator',
+          // title stying
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+
+        // action button
+        actions: const [
+          Icon(
+            Icons.snapchat,
+          )
+        ],
+
+        // action button padding
+        actionsPadding: const EdgeInsets.only(right: 8),
+      ),
+
+      // drawer
+      drawer: const Icon(Icons.grid_3x3),
+
+      // body
+
+      body: Container(
+        decoration: const BoxDecoration(color: Colors.grey),
+        padding: const EdgeInsets.all(10),
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          decoration: const BoxDecoration(color: Colors.white),
+          height: 550,
+          child: ListView.separated(
+              itemBuilder: (context, index) => Container(
+                    height: 140,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: containerColor[index],
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+              separatorBuilder: (context, index) => const SizedBox(height: 10,),
+              itemCount: containerColor.length),
+        ),
+      ),
+    );
+  }
+}
