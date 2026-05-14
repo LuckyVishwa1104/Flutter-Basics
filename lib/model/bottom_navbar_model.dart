5e6003f41ec55400e3b5028d1de0db26bba1a041
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-05-14 22:36:23 +0530

    feat(model): add model for bottom navbar items

---
+++ lib/model/bottom_navbar_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BottomNavbarModel {
+  final IconData icon;
+  final IconData selectedIcon;
+  final String label;
+
+  BottomNavbarModel({
+    required this.icon,
+    required this.selectedIcon,
+    required this.label,
+  });
+}
